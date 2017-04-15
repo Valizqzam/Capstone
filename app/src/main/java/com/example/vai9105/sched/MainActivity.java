8c8c66296cd4ec6b6b9d5665e83e3fdc1193247f
Andrew Peacock <andrewpeacock04@gmail.com>
2017-04-14 23:03:07 -0400

    added the abiltiy to annotate classes

--- app/src/main/java/com/example/vai9105/sched/MainActivity.java
+++ app/src/main/java/com/example/vai9105/sched/MainActivity.java
@@ -1,72 +1,85 @@
 package com.example.vai9105.sched;
 
-import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TabHost;
 
+import org.androidannotations.annotations.AfterInject;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity.class";
 
-    TabHost th;
+    @ViewById
+    FloatingActionButton fab;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    @ViewById
+    Toolbar toolbar;
+
+    @ViewById
+    TabHost tabs;
+    
+    @AfterInject
+    void afterInject(){
+        //data calls for main activity here
+    }
+    
+    @AfterViews
+    void afterViews(){
+        Log.d(TAG, "Creating the main Activities view");
+
         setSupportActionBar(toolbar);
 
-        th = (TabHost) findViewById(R.id.tabs);
-
         //tab1
-        th.setup();
-        TabHost.TabSpec tabSpec1 = th.newTabSpec("tab1");
+        tabs.setup();
+        TabHost.TabSpec tabSpec1 = tabs.newTabSpec("tab1");
         tabSpec1.setIndicator("Monday");
         tabSpec1.setContent(R.id.Monday);
-        th.addTab(tabSpec1);
+        tabs.addTab(tabSpec1);
 
         //tab2
-        th.setup();
-        TabHost.TabSpec tabSpec2 = th.newTabSpec("tab2");
+        tabs.setup();
+        TabHost.TabSpec tabSpec2 = tabs.newTabSpec("tab2");
         tabSpec2.setIndicator("Tuesday");
         tabSpec2.setContent(R.id.Tuesday);
-        th.addTab(tabSpec2);
+        tabs.addTab(tabSpec2);
 
         //tab3
-        th.setup();
-        TabHost.TabSpec tabSpec3 = th.newTabSpec("tab3");
+        tabs.setup();
+        TabHost.TabSpec tabSpec3 = tabs.newTabSpec("tab3");
         tabSpec3.setIndicator("Wednesday");
         tabSpec3.setContent(R.id.Wednesday);
-        th.addTab(tabSpec3);
-
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        tabs.addTab(tabSpec3);
     }
+
+    @Click(R.id.fab)
+    void floatingActionButtonClicked(View view){
+        Snackbar.make(view, "Replace witabs your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate tabse menu; tabsis adds items to tabse action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
+        // Handle action bar item clicks here. tabse action bar will
+        // automatically handle clicks on tabse Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
@@ -74,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
